2652938024701c50cef62ebd289d5d318d6eddd0
luohuidong <luohuidong01@126.com>
2024-01-05 23:01:54 +0800

    feat: support frontmatter

--- src/changeMarkdownContent.ts
+++ src/changeMarkdownContent.ts
@@ -3,6 +3,7 @@ import remarkParse from "remark-parse";
 import type { Root } from "mdast";
 import { visit } from "unist-util-visit";
 import remarkStringify from "remark-stringify";
+import remarkFrontmatter from "remark-frontmatter";
 
 const startRegExp = /^:::\sraw\n$/;
 const endRegExp = /^\n:::$/;
@@ -10,17 +11,20 @@ const endRegExp = /^\n:::$/;
 export async function changeMarkdownContent(content: string) {
   const result = await unified()
     .use(remarkParse)
+    .use(remarkFrontmatter)
     .use(() => (mdast: Root) => {
-      visit(mdast, "paragraph", (node) => {
-        visit(node, "text", (node) => {
-          if (startRegExp.test(node.value)) {
-            node.value = node.value + "<ClientOnly>\r\n" + '<div class="vp-raw">';
-          }
+      visit(mdast, ["paragraph"], (node) => {
+        if (node.type === "paragraph") {
+          visit(node, "text", (node) => {
+            if (startRegExp.test(node.value)) {
+              node.value = node.value + '<div class="vp-raw">';
+            }
 
-          if (endRegExp.test(node.value)) {
-            node.value = "</div>" + "\r\n</ClientOnly>" + node.value;
-          }
-        });
+            if (endRegExp.test(node.value)) {
+              node.value = "</div>" + node.value;
+            }
+          });
+        }
       });
     })
     .use(remarkStringify)
